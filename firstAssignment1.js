72b0ca04636c9d05829f5d7ad8a707a5e7d06eec
shreyautekar12 <utekarshreya.888@gmail.com>
2025-07-16 22:34:24 +0530

    Add files via upload

---
+++ firstAssignment1.js
@@ -0,0 +1,7 @@
+let msg1="Hello, this is my first Node.js peogram!";
+let msg2="I am excited to learn more about Node.js";
+let msg3=`${msg1+msg2}`;
+
+console.log(msg1);
+console.log(msg2);
+console.log(msg3);
---
+++ firstAssignment2.js
@@ -0,0 +1,7 @@
+let sentence1="This is the first sentence";
+let sentence2="This is the second sentence";
+let sentence3="This is the third sentence";
+
+console.log("Number of Characters in sentence 1:", sentence1.length);
+console.log("Number of Characters in sentence 2:", sentence2.length);
+console.log("Number of Characters in sentence 3:", sentence3.length);
